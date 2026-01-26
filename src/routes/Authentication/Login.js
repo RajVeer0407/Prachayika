ff5cd43900bdeb38112019d57a847faa8cc81467
Titan antony <antonytitan96@gmail.com>
2026-01-26 14:52:48 +0300

    Chore: Add a toggle icon to show password

--- src/routes/Authentication/Login.js
+++ src/routes/Authentication/Login.js
@@ -1,26 +1,41 @@
-import React from "react";
+import React, {useState} from "react";
 import { Link } from "react-router-dom";
 import "./auth.css";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
 
 const LoginPage = () => {
+  const [showPassword, setShowPassword] = useState(false);
+
   return (
     <div className="auth-wrapper">
       <div className="auth-card">
         <h2 className="auth-title">Welcome Back</h2>
         <p className="auth-subtitle">Login to your account</p>
 
-        <form className="auth-form">
+         <form className="auth-form">
           <input
             type="email"
             placeholder="Email address"
             className="auth-input"
           />
 
-          <input
-            type="password"
-            placeholder="Password"
-            className="auth-input"
-          />
+          {/* Password input with icon INSIDE */}
+          <div className="input-with-icon">
+            <input
+              type={showPassword ? "text" : "password"}
+              placeholder="Password"
+              className="auth-input auth-input-password"
+            />
+
+            <button
+              type="button"
+              className="password-icon"
+              onClick={() => setShowPassword((prev) => !prev)}
+              aria-label={showPassword ? "Hide password" : "Show password"}
+            >
+              {showPassword ? <FaEyeSlash /> : <FaEye />}
+            </button>
+          </div>
 
           <button type="submit" className="auth-button">
             Login
